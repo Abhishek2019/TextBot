4b8310c67bb7308a2d2f7f8246358913ba371057
Abhishek2019 <abhishekw13@gmail.com>
2017-11-11 19:44:45 +0530

    Abhishek2019: play music, greet user features updated: 11-11-17

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,71 +1,112 @@
 import os
 import random
 import webbrowser
-from pygame import mixer
-
 
 
 def file2list(filename):
 
     flist=[]
     fr = open(filename)
-
     for line in fr.readlines():
         flist.append(line.strip())
     return flist
 
 
+def write2file(filename,str):
 
+    fr=open(filename,'a')
+    str=str+"\n"
+    fr.write(str)
+
+
+def mp3gen():
+
+    for root, dirs, files in os.walk('/home'):
+        for filename in files:
+            if os.path.splitext(filename)[1] == ".mp3":
+                yield os.path.join(root, filename)
+
+def playmusic(str):
+
+    print("playing....")
+    webbrowser.open(str)
+
+
+def showsongslist():
+
+    for mp3file in mp3gen():
+        print(mp3file)
+
+
+def robospeak(str):
+
+    os.system("espeak '" + str + "'")
+    print(str)
+
+
+def humaninput():
+
+    print("you : ", end="")
+    return (input().strip())
 
 
 robo=random.choice(("Hello  How are you feeling today?","Hi How are you feeling today?","whats up?"))
-os.system("espeak '"+robo+"'")
+robospeak(robo)
 
-print(robo)
 
-print("you : ",end="")
-human=input().strip()
+human= humaninput()
+
 
 while 1:
-    if(human in ["I am fine","Thank you","nice day"]):
+    if(human in file2list("../dataset/h_greeting_reply.txt")):
         robo=random.choice(("Thats Great so what would you like me to do?","Pleasure to hear that  so what would you like me to do?"))
-        os.system("espeak '"+robo+"'")
-        print(robo)
+        robospeak(robo)
+    elif("nice" in human.strip().split() or "good" in human.strip().split() or "fine" in human.strip().split()):
+        write2file("../dataset/h_greeting_reply.txt",human)
+
+        robo=random.choice(("Thats Great so what would you like me to do?","Pleasure to hear that  so what would you like me to do?"))
+        robospeak(robo)
+
     elif(human in ["run again"]):
-        robo=random.choice(("so what would you like me to do?"))
-        os.system("espeak '"+robo+"'")
-        print(robo)
+        robo=random.choice(("so what would you like me again to do?","would you like me to to execute another task?"))
+        robospeak(robo)
+    else:
+        robo=random.choice(("It's ok don't get depressed  so whatwould you like me to do?"))
+        robospeak(robo)
 
 
-    print("you : ", end="")
-    human = input().strip()
-
+    human = humaninput()
 
 
     if(human in file2list("../dataset/music.txt")):
         robo=random.choice(("Here comes list of songs :","Here comes list","choose your song"))
-        os.system("espeak '"+robo+"'")
-        print(robo)
+        robospeak(robo)
+
+        showsongslist()
+        human=humaninput()
+        playmusic(human)
 
 
-        def mp3gen():
-            for root, dirs, files in os.walk('/home'):
-                for filename in files:
-                    if os.path.splitext(filename)[1] == ".mp3":
-                        yield os.path.join(root, filename)
+    elif("play" in human.strip().split() and ("song" in human.strip().split() or "mp3" in human.strip().split())):
+        write2file("../dataset/music.txt",human)
+
+        robo = random.choice(("Here comes list of songs :", "Here comes list", "choose your song"))
+        robospeak(robo)
+
+        showsongslist()
+        human=humaninput()
+        playmusic(human)
 
 
-        for mp3file in mp3gen():
-            print(mp3file)
-        print("you : ",end="")
-        human=input().strip()
-        print("playing....")
-
-        webbrowser.open(human)
-    elif(human in ["stop","bye"]):
+    elif(human in ["stop","bye","no"]):
+        robo = random.choice(("Thank you","I will take your leave","Bye sir","It was nice meeting you"))
+        robospeak(robo)
         break
+
     human="run again"
-    continue
+
+
+
 
 
 
