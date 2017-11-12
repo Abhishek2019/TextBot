9be97b17c90660f5168b3d84678201e15f8494da
Abhishek2019 <abhishekw13@gmail.com>
2017-11-12 10:55:16 +0530

    Abhishek2019: song feature completed: 12-11-17

--- src/chatbot.py
+++ src/chatbot.py
@@ -20,22 +20,30 @@ def write2file(filename,str):
 
 
 def mp3gen():
-
+    fpath=[]
+    fname=[]
     for root, dirs, files in os.walk('/home'):
         for filename in files:
             if os.path.splitext(filename)[1] == ".mp3":
-                yield os.path.join(root, filename)
 
-def playmusic(str):
+                fpath.append(os.path.join(root, filename))
+                fname.append(filename)
+    return fpath,fname
+
+def playmusic(fpath,str):
+
 
     print("playing....")
-    webbrowser.open(str)
+    webbrowser.open(fpath[int(str)-1])
 
 
 def showsongslist():
-
-    for mp3file in mp3gen():
-        print(mp3file)
+    no=1
+    fpath,fname=mp3gen()
+    for mp3file in fname:
+        print(str(no)+" "+mp3file)
+        no+=1
+    return fpath
 
 
 def robospeak(str):
@@ -58,6 +66,7 @@ human= humaninput()
 
 
 while 1:
+
     if(human in file2list("../dataset/h_greeting_reply.txt")):
         robo=random.choice(("Thats Great so what would you like me to do?","Pleasure to hear that  so what would you like me to do?"))
         robospeak(robo)
@@ -68,10 +77,10 @@ while 1:
         robospeak(robo)
 
     elif(human in ["run again"]):
-        robo=random.choice(("so what would you like me again to do?","would you like me to to execute another task?"))
+        robo=random.choice(("so what would you like me again to do?","would you like me to to execute any task?"))
         robospeak(robo)
     else:
-        robo=random.choice(("It's ok don't get depressed  so whatwould you like me to do?"))
+        robo=random.choice(("It's ok don't get too depressed  so whatwould you like me to do?"))
         robospeak(robo)
 
 
@@ -82,20 +91,20 @@ while 1:
         robo=random.choice(("Here comes list of songs :","Here comes list","choose your song"))
         robospeak(robo)
 
-        showsongslist()
+        fpath=showsongslist()
         human=humaninput()
-        playmusic(human)
+        playmusic(fpath,human)
 
 
-    elif("play" in human.strip().split() and ("song" in human.strip().split() or "mp3" in human.strip().split())):
+    elif(("show" in human.strip().split() or "play" in human.strip().split()) and ("songs" in human.strip().split() or "song" in human.strip().split() or "mp3" in human.strip().split())):
         write2file("../dataset/music.txt",human)
 
         robo = random.choice(("Here comes list of songs :", "Here comes list", "choose your song"))
         robospeak(robo)
 
-        showsongslist()
+        fpath=showsongslist()
         human=humaninput()
-        playmusic(human)
+        playmusic(fpath,human)
 
 
     elif(human in ["stop","bye","no"]):
